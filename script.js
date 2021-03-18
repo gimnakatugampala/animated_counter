50abdbb0ef8b0266139eaf45c2c93b80b770e8c0
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-03-18 21:26:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll('.counter');
+const speed = 200;
+
+counters.forEach(counter =>{
+    const updateCount = () =>{
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+
+        const inc = target / speed;
+
+        console.log(inc);
+
+        if(count < target){
+            counter.innerText = count + inc;
+            setTimeout(updateCount,1);
+        }else{
+            count.innerText = target;
+        }
+    }
+    updateCount();
+})
